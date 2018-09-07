88268480ab83a7386aac1889c61cf92afadb56f0
Wenting <wenttzzheng@gmail.com>
2018-09-07 11:22:42 -0700

    Adds code to normalize data for knn in data mining

--- projects/CSC422 knn.py
+++ projects/CSC422 knn.py
@@ -53,3 +53,37 @@ for data in data16['ALL-STAR']:
 y_train = np.append(y_train14, y_train15, axis = 0)
 y_train = np.append(y_train, y_train16, axis = 0)
 
+
+# Separate x and y for the testing dataset
+x_test = np.array(test17.loc[:, test17.columns[2:-1]])
+y_test = []
+for data in test17['ALL-STAR']:
+    if data == 'Yes':
+        y_test.append(1)
+    else:
+        y_test.append(0)
+# print(x_train16)
+#####################################################
+#           This part is for normalization          #
+#####################################################
+# normalize the training datasets
+norm14 = (x_train14 - x_train14.min(axis=0)) / (x_train14.max(axis=0) - x_train14.min(axis=0))
+norm15 = (x_train15 - x_train15.min(axis=0)) / (x_train15.max(axis=0) - x_train15.min(axis=0))
+norm16 = (x_train16 - x_train16.min(axis=0)) / (x_train16.max(axis=0) - x_train16.min(axis=0))
+norm_all = (x_train - x_train.min(axis=0)) / (x_train.max(axis=0) - x_train.min(axis=0))
+
+# normalize the testing dataset
+normtest14 = (x_test - x_train14.min(axis=0)) / (x_train14.max(axis=0) - x_train14.min(axis=0))
+normtest15 = (x_test - x_train15.min(axis=0)) / (x_train15.max(axis=0) - x_train15.min(axis=0))
+normtest16 = (x_test - x_train16.min(axis=0)) / (x_train16.max(axis=0) - x_train16.min(axis=0))
+normtest_all = (x_test - x_train.min(axis=0)) / (x_train.max(axis=0) - x_train.min(axis=0))
+
+# from sklearn.preprocessing import StandardScaler
+# scaler = StandardScaler()
+# scaler.fit(x_train)
+#
+# norm_all = scaler.transform(x_train)
+# normtest_all = scaler.transform(x_test)
+# print(norm_all)
+
+# print(y_train)
